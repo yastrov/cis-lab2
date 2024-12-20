e3b9f77274e54f42152f0e67ebb66a0acd8f7d70
yastrov <113824052+yastrov@users.noreply.github.com>
2024-12-20 11:37:22 +0500

    Update Jenkinsfile.groovy

--- ci/Jenkinsfile.groovy
+++ ci/Jenkinsfile.groovy
@@ -1,1 +1,10 @@
-
+pipeline {
+    agent { docker { image 'node:22.12.0-alpine3.21' } }
+    stages {
+        stage('build') {
+            steps {
+                sh 'node --version'
+            }
+        }
+    }
+}
